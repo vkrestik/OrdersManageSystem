94493f1ea70a064a0c5e75485005598de9f3f21d
vkrestik <77360115+vkrestik@users.noreply.github.com>
2025-12-12 05:49:00 +0300

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,17 @@
+export let state = {
+    //nextBlockId: 0,
+    currentBlock: null,
+    currentBlockInfo: {
+        duration: null,
+        comment: null,
+        contractId:null, 
+        taskName:null
+    },
+    blocksIdArray: new Array(),
+    user: {
+        userId: null,
+        name: null,
+        surname: null,
+        role: null
+    }
+}
